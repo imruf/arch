796ef99fea4ee3d08994461a9fa0f97e422bf6c0
imrufbd <imrufbd@gmail.com>
2019-06-14 11:40:26 +0600

    dwm-statusbar

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -1,10 +1,10 @@
 #! /bin/bash 
-compton --config ~/.config/i3/compton.conf &
+xfce4-power-manager &
+compton --config ~/.config/compton/compton.conf &
 numlockx on &
 xinput set-prop 14 284 1
 feh --bg-scale ~/Pictures/feh/wallpaper/galaxy.jpg
 redshift &
-dwmstatus &
 
 wifi(){
 	#ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
@@ -20,11 +20,11 @@ wifi(){
 	done < <(iw "$if" link)
 
 	#echo -e "$SSID $SIGNAL $ip"
-	echo -e "$SSID"
+	echo -e "$SSID "
 }
 
 dte(){
-  dte="$(date +"%d %b %a|%H:%M")"
+  dte="$(date +"  %d %b, %a |  %H:%M")"
   echo -e "$dte"
 }
 
@@ -38,15 +38,16 @@ bat(){
     battery="$(cat /sys/class/power_supply/BAT0/capacity)"
     timer="$(acpi -b | grep "Battery" | awk '{print $5}' | cut -c 1-5)"
     if [ "${status}" == 1 ]; then
-      echo -ne "${color6}BAT ${color0}ON ${battery}%"
+      echo -ne "${color6} ${color0}🗲 ${battery}%"
     else
-      echo -ne "${color6}BAT ${color0}${battery}%"
+      echo -ne "${color6} ${color0}${battery}%"
     fi
 }
 
 mem(){
-  mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
-  echo -e "$mem"
+  mem=`free | awk '/Mem/ {printf "%d MiB\n", $3 / 1024.0}'`
+  #mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
+  echo -e " $mem"
 }
 
 cpu(){
@@ -56,19 +57,19 @@ cpu(){
   read cpu a b c idle rest < /proc/stat
   total=$((a+b+c+idle))
   cpu=$((100*( (total-prevtotal) - (idle-previdle) ) / (total-prevtotal) ))
-  echo -e "$cpu% cpu"
+  echo -e "  $cpu%"
 }
 
 vol() {
 	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
 	if test "$volume" -gt 0
 	then
-		echo -e "\uE05D${volume}"
+		echo -e " ${volume}"
 	else
-		echo -e "Mute"
+		echo -e ""
 	fi
 }
 while true; do
-    xsetroot -name "$(cpu)|$(mem)|$(vol)|$(bat)|$(dte)|$(wifi)"
+    xsetroot -name "$(cpu) | $(mem)| $(vol) | $(bat) | $(dte) | $(wifi)"
      sleep 1m    # Update time every ten seconds
 done &
