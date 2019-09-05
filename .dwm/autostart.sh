4548d5c77c2d4e3d7ea217822037db46f615cb03
imrufbd <imrufbd@gmail.com>
2019-09-06 00:47:19 +0600

    .dwm

--- .dwm/autostart.sh
+++ .dwm/autostart.sh
@@ -8,25 +8,26 @@ feh --bg-scale ~/Pictures/feh/dwm/nano-schematic-infraorange-3840x2400.jpg
 redshift &
 #conky &
 
+
 wifi(){
 	#ip=$(ip route get 8.8.8.8 2>/dev/null|grep -Eo 'src [0-9.]+'|grep -Eo '[0-9.]+')
 
-	if=wlp4s0
-		while IFS=$': \t' read -r label value
-		do
-			case $label in SSID) SSID=$value
-				;;
-	#	signal) SIGNAL=$value
-	#			;;
-		esac
-	done < <(iw "$if" link)
-
-	#echo -e "$SSID $SIGNAL $ip"
-	echo -e "$SSID"
+#	if=wlp4s0
+#		while IFS=$': \t' read -r label value
+#		do
+#			case $label in SSID) SSID=$value
+#				;;
+#		#signal) SIGNAL=$value
+#		#		;;
+#		esac
+#	done < <(iw "$if" link)
+    ssid=$(iwgetid -r)
+    sig=$(grep "^\s*w" /proc/net/wireless | awk '{ print "", int($3 * 100 / 70) "%" }')
+	echo -e "$ssid $sig"
 }
 
 dte(){
-  dte="$(date +" %d %b, %a| %H:%M")"
+  dte="$(date +" %d %b, %a|%H:%M  " )"
   echo -e "$dte"
 }
 
@@ -34,7 +35,6 @@ dte(){
 #  upd=`checkupdates | wc -l`
 #  echo -e "⟳ $upd updates"
 #}
-
 bat(){
     status="$(cat /sys/class/power_supply/AC0/online)"
     battery="$(cat /sys/class/power_supply/BAT0/capacity)"
@@ -42,14 +42,34 @@ bat(){
     if [ "${status}" == 1 ]; then
       echo -ne "${color6} ${color0}🗲 ${battery}%"
     else
-      echo -ne "${color6} ${color0}${battery}%"
+	for x in /sys/class/power_supply/BAT0/capacity;
+	do
+	case "$(cat $x)" in
+		100|9[0-9])	echo "" ;;
+		8[0-9]|7[0-9])	echo "" ;;
+		6[0-9]|5[0-9])	echo "" ;;
+		4[0-9]|3[0-9])	echo "" ;;
+		*)		echo "" ;;
+	esac
+	done
     fi
 }
+#bat(){
+#    status="$(cat /sys/class/power_supply/AC0/online)"
+#    battery="$(cat /sys/class/power_supply/BAT0/capacity)"
+#    timer="$(acpi -b | grep "Battery" | awk '{print $5}' | cut -c 1-5)"
+#    if [ "${status}" == 1 ]; then
+#      echo -ne "${color6} ${color0}🗲 ${battery}%"
+#    else
+#      echo -ne "${color6} ${color0}${battery}%"
+#    fi
+#}
 
 mem(){
   mem=`free | awk '/Mem/ {printf "%d MiB\n", $3 / 1024.0}'`
   #mem=`free | awk '/Mem/ {printf "%d MiB/%d MiB\n", $3 / 1024.0, $2 / 1024.0 }'`
-  echo -e " $mem"
+  #echo -e " $mem"
+  echo -e " $mem"
 }
 
 cpu(){
@@ -63,15 +83,20 @@ cpu(){
 }
 
 vol() {
-	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
-	if test "$volume" -gt 0
-	then
-		echo -e " ${volume}"
-	else
-		echo -e ""
-	fi
+    volume=$(pavolume.sh)
+    echo -e $volume
 }
+
+#vol() {
+#	volume="$(amixer get Master | tail -n1 | sed -r 's/.*\[(.*)%\].*/\1/')"
+#	if test "$volume" -gt 0
+#	then
+#		echo -e " ${volume}"
+#	else
+#		echo -e ""
+#	fi
+#}
 while true; do
-    xsetroot -name "$(cpu)|$(mem)|$(vol)|$(bat)|$(dte)|$(wifi)"
+    xsetroot -name "$(cpu)|$(mem)|$(vol)|$(bat)|$(dte)~$(wifi)"
      sleep 1m    # Update time every ten seconds
 done &
