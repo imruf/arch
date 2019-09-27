dec96b3d4226981ad8aa2ff9570aea6731871c28
imrufbd <imrufbd@gmail.com>
2019-09-27 20:34:44 +0600

    dwm

--- dwm/config.h
+++ dwm/config.h
@@ -81,6 +81,7 @@ static const char *fmcmd[] = { "pcmanfm", NULL };
 static const char *bcmd[] = { "qutebrowser", NULL };
 static const char *pmenu[] = { "powermenu", NULL };
 static const char *dweb[] = { "dweb", NULL };
+static const char *rbang[] = { "rbang", NULL };
 static const char *ips[] = { "i3exit", "suspend", NULL };
 static const char *ipr[] = { "i3exit", "reboot", NULL };
 static const char *iph[] = { "i3exit", "shutdown", NULL };
@@ -151,6 +152,7 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,             XK_e,              quit,           {0} },
 	{ MODKEY|ShiftMask,             XK_x,              spawn,          {.v = pmenu } },
 	{ MODKEY|ControlMask,           XK_i,              spawn,          {.v = dweb } },
+	{ MOD2,                         XK_i,              spawn,          {.v = rbang } },
 	{ MODKEY|ShiftMask,             XK_s,              spawn,          {.v = ips } },
 	{ MODKEY|ShiftMask,             XK_r,              spawn,          {.v = ipr } },
 	{ MODKEY|ShiftMask,             XK_h,              spawn,          {.v = iph } },
