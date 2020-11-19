6307fb9c63bf4b7674ee891f8f8c62aa6220bc8d
imrufbd <imrufbd@gmail.com>
2020-11-19 21:23:54 +0600

    dwm-6.2

--- dwm-6.2/config.def.h
+++ dwm-6.2/config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 0;        /* border pixel of windows */
-static const unsigned int gappx     = 1;        /* gap pixel between windows */
+static const unsigned int gappx     = 2;        /* gap pixel between windows */
 static const unsigned int snap      = 22;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
