4eb65fe7b1451bc321b022856020258200f586c8
imrufbd <imrufbd@gmail.com>
2020-11-20 23:52:33 +0600

    dwm-6.2

--- dwm-6.2/config.h
+++ dwm-6.2/config.h
@@ -73,10 +73,10 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
-	{ "󰙀",      tile },    /* first entry is default */
-	{ "󱂬",      NULL },    /* no layout function means floating behavior */
-	{ "󰊓",      monocle },
-	{ "󰕴",      bstack },
+	{ " 󰙀 ",      tile },    /* first entry is default */
+	{ " 󱂬",      NULL },    /* no layout function means floating behavior */
+	{ " 󰊓",      monocle },
+	{ " 󰕴",      bstack },
 };
 
 /* key definitions */
