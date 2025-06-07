fac51a92473034462d9be55a8a4a7c9d98a400dd
imrufbd <imrufbd@gmail.com>
2025-06-07 14:35:02 +0600

    update

---
+++ .config/yazi/init.lua
@@ -0,0 +1,15 @@
+require("yaziline"):setup({
+  color = "#98c379", -- main theme color
+  separator_style = "empty", -- "angly" | "curvy" | "liney" | "empty"
+  separator_open = "",
+  separator_close = "",
+  separator_open_thin = "",
+  separator_close_thin = "",
+  separator_head = "",
+  separator_tail = "",
+  select_symbol = "",
+  yank_symbol = "󰆐",
+  filename_max_length = 24, -- truncate when filename > 24
+  filename_truncate_length = 6, -- leave 6 chars on both sides
+  filename_truncate_separator = "..." -- the separator of the truncated filename
+})
